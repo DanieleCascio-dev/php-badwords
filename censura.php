f7d3c220d7ae9ad2a2589a99ad67521c7a3b6182
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2023-12-11 15:50:31 +0100

    add: html structure with php in censura.php

--- censura.php
+++ censura.php
@@ -1,9 +1,28 @@
 <?php
 
 $paragraph = $_GET['paragraph'];
-echo $paragraph;
+$paragraph_lenght = strlen($paragraph);
+// echo $paragraph;
 $censored = $_GET['censored'];
-echo $censored;
+$censored_paragraph = str_replace($censored,'***',$paragraph);
+$censored_paragraph_lenght = strlen($censored_paragraph);
+// echo $censored;
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>La tua censura (bello il 2023)</title>
+</head>
+<body>
+  <h2>Ecco il tuo risultato:</h2>
+  <p>Questo è il paragrafo che hai deciso di censurare: <?php echo $paragraph; ?>. <br>
+    Continene <?php echo $paragraph_lenght; ?> caratteri.</p>
+    <p>Questo è il paragrafo con la censura sulla parola <?php echo $censored; ?>: <?php echo $censored_paragraph; ?>. <br>
+      Continene <?php echo $censored_paragraph_lenght ?> caratteri</p>
+</body>
+</html>
