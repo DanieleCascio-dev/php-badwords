75d7962253787caeb4fd87f69406f234d7d0330b
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2023-12-11 15:27:56 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>php badwords</title>
+</head>
+<body>
+
+<h2>Censuriamoci</h2>
+  <form action="censura.php" method="GET">
+    <label for="paragraph">Scrivi un paragrafo</label>
+    <input type="text" id="paragraph" name="paragraph">
+  </form>
+</body>
+</html>
